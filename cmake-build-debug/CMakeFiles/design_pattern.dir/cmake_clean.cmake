06a82aa0e98d2e11616d0d2a2374d407527c0d62
mjzheng <mjzheng@tencent.com>
2020-08-10 09:56:30 +0800

    add files

---
+++ cmake-build-debug/CMakeFiles/design_pattern.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/design_pattern.dir/main.cpp.o"
+  "design_pattern"
+  "design_pattern.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/design_pattern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
